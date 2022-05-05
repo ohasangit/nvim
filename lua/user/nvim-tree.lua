6e017c81c424cd89bf861e2a06dc81a0b719f282
Omar Hasan <omar.a.hasan@lmco.com>
2022-05-04 22:42:36 -0600

    Don't disable netrw. Helps viewing files on network/url

--- lua/user/nvim-tree.lua
+++ lua/user/nvim-tree.lua
@@ -35,7 +35,7 @@ local tree_cb = nvim_tree_config.nvim_tree_callback
 
 nvim_tree.setup { -- BEGIN_DEFAULT_OPTS
   auto_reload_on_write = true,
-  disable_netrw = true,
+  disable_netrw = false,
   hijack_cursor = false,
   hijack_netrw = true,
   hijack_unnamed_buffer_when_opening = false,
