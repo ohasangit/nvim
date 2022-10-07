48999946f900cecf3852bbb8268c8ece9818a64a
Omar Hasan <omar.hasan@torc.ai>
2022-10-07 09:33:31 -0600

    Added web-tools plugin

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -99,6 +99,9 @@ return packer.startup(function(use)
     end
   }
 
+  -- Web Development
+  use "ray-x/web-tools.nvim"
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
