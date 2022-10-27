b06f42d288a96477a36e764090f131e429667485
Omar Hasan <omar.hasan@torc.ai>
2022-10-27 14:44:06 -0600

    Using more explicit configuration of project-nvim

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -75,7 +75,7 @@ return packer.startup(function(use)
   use "nvim-telescope/telescope-media-files.nvim"
   use { "nvim-telescope/telescope-fzf-native.nvim", run = "make" }
   use { "dhruvmanila/telescope-bookmarks.nvim", requires = { "tami5/sqlite.lua", "tyru/open-browser.vim" } }
-  use { "ahmedkhalf/project.nvim", config = function() require("project_nvim").setup {} end }
+  use { "ahmedkhalf/project.nvim" }
 
   -- Treesitter
   use {
--- lua/user/project-nvim.lua
+++ lua/user/project-nvim.lua
@@ -1,22 +1,23 @@
 local status_ok, project_nvim = pcall(require, "project_nvim")
 if not status_ok then
+  print "Could not load project_nvim"
   return
 end
 
 project_nvim.setup {
   -- Manual mode doesn't automatically change your root directory, so you have
   -- the option to manually do so using `:ProjectRoot` command.
-  manual_mode = true,
+  manual_mode = false,
 
   -- Methods of detecting the root directory. **"lsp"** uses the native neovim
   -- lsp, while **"pattern"** uses vim-rooter like glob pattern matching. Here
   -- order matters: if one is not detected, the other is used as fallback. You
   -- can also delete or rearangne the detection methods.
-  detection_methods = { "lsp", "pattern" },
+  detection_methods = { "pattern" },
 
   -- All the patterns used to detect root dir, when **"pattern"** is in
   -- detection_methods
-  patterns = { ".git", "_darcs", ".hg", ".bzr", ".svn", "Makefile", "package.json" },
+  patterns = { ".git" },
 
   -- Table of lsp clients to ignore by name
   -- eg: { "efm", ... }
