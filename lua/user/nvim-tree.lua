e2f540616daf3d32931b10efdc54a7a0b35a9c4d
Omar Hasan <omar.hasan@torc.ai>
2022-11-13 17:40:53 -0700

    Cleaned up nvim-tree settings

--- lua/user/nvim-tree.lua
+++ lua/user/nvim-tree.lua
@@ -12,28 +12,13 @@ end
 
 local tree_cb = nvim_tree_config.nvim_tree_callback
 
-nvim_tree.setup { -- BEGIN_DEFAULT_OPTS
-  auto_reload_on_write = true,
-  disable_netrw = false,
-  hijack_cursor = false,
-  hijack_netrw = true,
-  hijack_unnamed_buffer_when_opening = false,
-  ignore_buffer_on_setup = false,
-  open_on_setup = false,
-  open_on_setup_file = false,
-  open_on_tab = false,
-  sort_by = "name",
-  update_cwd = true,
+nvim_tree.setup {
+  sync_root_with_cwd = true,
+  reload_on_bufenter = true,
   view = {
     width = 50,
-    hide_root_folder = false,
     side = "right",
-    preserve_window_proportions = false,
-    number = false,
-    relativenumber = false,
-    signcolumn = "yes",
     mappings = {
-      custom_only = false,
       list = {
         { key = { "l", "<CR>", "o" }, cb = tree_cb "edit" },
         { key = "h", cb = tree_cb "close_node" },
@@ -42,106 +27,33 @@ nvim_tree.setup { -- BEGIN_DEFAULT_OPTS
     },
   },
   renderer = {
-    indent_markers = {
-      enable = false,
-      icons = {
-        corner = "└ ",
-        edge = "│ ",
-        none = "  ",
-      },
-    },
     icons = {
-      webdev_colors = true,
+      git_placement = 'after',
       glyphs = {
-        default = "",
-        symlink = "",
         git = {
           unstaged = "",
           staged = "S",
-          unmerged = "",
-          renamed = "➜",
-          deleted = "",
           untracked = "U",
-          ignored = "◌",
-        },
-        folder = {
-          default = "",
-          open = "",
-          empty = "",
-          empty_open = "",
-          symlink = "",
         },
       },
     },
   },
-  hijack_directories = {
-    enable = true,
-    auto_open = true,
-  },
   update_focused_file = {
-    enable = false,
-    update_cwd = false,
-    ignore_list = {},
-  },
-  ignore_ft_on_setup = {},
-  system_open = {
-    cmd = "",
-    args = {},
+    enable = true,
   },
   diagnostics = {
-    enable = false,
+    enable = true,
     show_on_dirs = false,
-    icons = {
-      hint = "",
-      info = "",
-      warning = "",
-      error = "",
-    },
   },
   filters = {
-    dotfiles = false,
-    custom = {},
-    exclude = {},
-  },
-  git = {
-    enable = true,
-    ignore = true,
-    timeout = 400,
+    dotfiles = true,
   },
   actions = {
-    use_system_clipboard = true,
-    change_dir = {
-      enable = true,
-      global = false,
-      restrict_above_cwd = false,
-    },
     open_file = {
-      quit_on_open = false,
       resize_window = false,
       window_picker = {
         enable = true,
-        chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890",
-        exclude = {
-          filetype = { "notify", "packer", "qf", "diff", "fugitive", "fugitiveblame" },
-          buftype = { "nofile", "terminal", "help" },
-        },
       },
     },
   },
-  trash = {
-    cmd = "trash",
-    require_confirm = true,
-  },
-  log = {
-    enable = false,
-    truncate = false,
-    types = {
-      all = false,
-      config = false,
-      copy_paste = false,
-      diagnostics = false,
-      git = false,
-      profile = false,
-    },
-  },
-} -- END_DEFAULT_OPTS
+}
