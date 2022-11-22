3ac36ca6954750309a214e38c9e0b211a7f00b10
Omar Hasan <omar.hasan@torc.ai>
2022-11-21 21:17:00 -0700

    Configure colorscheme

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -44,7 +44,6 @@ return packer.startup(function(use)
 
   -- Colorschemes
   use("folke/tokyonight.nvim")
-  use("marko-cerovac/material.nvim")
 
   -- cmp plugins
   use("hrsh7th/nvim-cmp") -- The completion plugin
