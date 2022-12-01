39c563af9c2df55110165f1406646675decab215
Omar Hasan <omar.hasan@torc.ai>
2022-11-30 21:39:15 -0700

    linting

--- lua/user/cmp.lua
+++ lua/user/cmp.lua
@@ -1,172 +1,171 @@
-local cmp_status_ok, cmp = pcall(require, "cmp")
+local cmp_status_ok, cmp = pcall(require, 'cmp')
 if not cmp_status_ok then
   return
 end
 
-local snip_status_ok, luasnip = pcall(require, "luasnip")
+local snip_status_ok, luasnip = pcall(require, 'luasnip')
 if not snip_status_ok then
   return
 end
 
-local format = require("cmp_git.format")
-local sort = require("cmp_git.sort")
+local format = require('cmp_git.format')
+local sort = require('cmp_git.sort')
 
-require("cmp_git").setup({
-    -- defaults
-    filetypes = { "gitcommit", "octo" },
-    remotes = { "upstream", "origin" }, -- in order of most to least prioritized
-    enableRemoteUrlRewrites = false, -- enable git url rewrites, see https://git-scm.com/docs/git-config#Documentation/git-config.txt-urlltbasegtinsteadOf
-    git = {
-        commits = {
-            limit = 100,
-            sort_by = sort.git.commits,
-            format = format.git.commits,
-        },
+require('cmp_git').setup({
+  -- defaults
+  filetypes = { 'gitcommit', 'octo' },
+  remotes = { 'upstream', 'origin' }, -- in order of most to least prioritized
+  enableRemoteUrlRewrites = false, -- enable git url rewrites, see https://git-scm.com/docs/git-config#Documentation/git-config.txt-urlltbasegtinsteadOf
+  git = {
+    commits = {
+      limit = 100,
+      sort_by = sort.git.commits,
+      format = format.git.commits,
     },
-    github = {
-        issues = {
-            fields = { "title", "number", "body", "updatedAt", "state" },
-            filter = "all", -- assigned, created, mentioned, subscribed, all, repos
-            limit = 100,
-            state = "open", -- open, closed, all
-            sort_by = sort.github.issues,
-            format = format.github.issues,
-        },
-        mentions = {
-            limit = 100,
-            sort_by = sort.github.mentions,
-            format = format.github.mentions,
-        },
-        pull_requests = {
-            fields = { "title", "number", "body", "updatedAt", "state" },
-            limit = 100,
-            state = "open", -- open, closed, merged, all
-            sort_by = sort.github.pull_requests,
-            format = format.github.pull_requests,
-        },
+  },
+  github = {
+    issues = {
+      fields = { 'title', 'number', 'body', 'updatedAt', 'state' },
+      filter = 'all', -- assigned, created, mentioned, subscribed, all, repos
+      limit = 100,
+      state = 'open', -- open, closed, all
+      sort_by = sort.github.issues,
+      format = format.github.issues,
     },
-    gitlab = {
-        issues = {
-            limit = 100,
-            state = "opened", -- opened, closed, all
-            sort_by = sort.gitlab.issues,
-            format = format.gitlab.issues,
-        },
-        mentions = {
-            limit = 100,
-            sort_by = sort.gitlab.mentions,
-            format = format.gitlab.mentions,
-        },
-        merge_requests = {
-            limit = 100,
-            state = "opened", -- opened, closed, locked, merged
-            sort_by = sort.gitlab.merge_requests,
-            format = format.gitlab.merge_requests,
-        },
+    mentions = {
+      limit = 100,
+      sort_by = sort.github.mentions,
+      format = format.github.mentions,
     },
-    trigger_actions = {
-        {
-            debug_name = "git_commits",
-            trigger_character = ":",
-            action = function(sources, trigger_char, callback, params, git_info)
-                return sources.git:get_commits(callback, params, trigger_char)
-            end,
-        },
-        {
-            debug_name = "gitlab_issues",
-            trigger_character = "#",
-            action = function(sources, trigger_char, callback, params, git_info)
-                return sources.gitlab:get_issues(callback, git_info, trigger_char)
-            end,
-        },
-        {
-            debug_name = "gitlab_mentions",
-            trigger_character = "@",
-            action = function(sources, trigger_char, callback, params, git_info)
-                return sources.gitlab:get_mentions(callback, git_info, trigger_char)
-            end,
-        },
-        {
-            debug_name = "gitlab_mrs",
-            trigger_character = "!",
-            action = function(sources, trigger_char, callback, params, git_info)
-                return sources.gitlab:get_merge_requests(callback, git_info, trigger_char)
-            end,
-        },
-        {
-            debug_name = "github_issues_and_pr",
-            trigger_character = "#",
-            action = function(sources, trigger_char, callback, params, git_info)
-                return sources.github:get_issues_and_prs(callback, git_info, trigger_char)
-            end,
-        },
-        {
-            debug_name = "github_mentions",
-            trigger_character = "@",
-            action = function(sources, trigger_char, callback, params, git_info)
-                return sources.github:get_mentions(callback, git_info, trigger_char)
-            end,
-        },
+    pull_requests = {
+      fields = { 'title', 'number', 'body', 'updatedAt', 'state' },
+      limit = 100,
+      state = 'open', -- open, closed, merged, all
+      sort_by = sort.github.pull_requests,
+      format = format.github.pull_requests,
     },
-  }
-)
+  },
+  gitlab = {
+    issues = {
+      limit = 100,
+      state = 'opened', -- opened, closed, all
+      sort_by = sort.gitlab.issues,
+      format = format.gitlab.issues,
+    },
+    mentions = {
+      limit = 100,
+      sort_by = sort.gitlab.mentions,
+      format = format.gitlab.mentions,
+    },
+    merge_requests = {
+      limit = 100,
+      state = 'opened', -- opened, closed, locked, merged
+      sort_by = sort.gitlab.merge_requests,
+      format = format.gitlab.merge_requests,
+    },
+  },
+  trigger_actions = {
+    {
+      debug_name = 'git_commits',
+      trigger_character = ':',
+      action = function(sources, trigger_char, callback, params, git_info)
+        return sources.git:get_commits(callback, params, trigger_char)
+      end,
+    },
+    {
+      debug_name = 'gitlab_issues',
+      trigger_character = '#',
+      action = function(sources, trigger_char, callback, params, git_info)
+        return sources.gitlab:get_issues(callback, git_info, trigger_char)
+      end,
+    },
+    {
+      debug_name = 'gitlab_mentions',
+      trigger_character = '@',
+      action = function(sources, trigger_char, callback, params, git_info)
+        return sources.gitlab:get_mentions(callback, git_info, trigger_char)
+      end,
+    },
+    {
+      debug_name = 'gitlab_mrs',
+      trigger_character = '!',
+      action = function(sources, trigger_char, callback, params, git_info)
+        return sources.gitlab:get_merge_requests(callback, git_info, trigger_char)
+      end,
+    },
+    {
+      debug_name = 'github_issues_and_pr',
+      trigger_character = '#',
+      action = function(sources, trigger_char, callback, params, git_info)
+        return sources.github:get_issues_and_prs(callback, git_info, trigger_char)
+      end,
+    },
+    {
+      debug_name = 'github_mentions',
+      trigger_character = '@',
+      action = function(sources, trigger_char, callback, params, git_info)
+        return sources.github:get_mentions(callback, git_info, trigger_char)
+      end,
+    },
+  },
+})
 
-require("luasnip/loaders/from_vscode").lazy_load()
+require('luasnip/loaders/from_vscode').lazy_load()
 
 local check_backspace = function()
-  local col = vim.fn.col "." - 1
-  return col == 0 or vim.fn.getline("."):sub(col, col):match "%s"
+  local col = vim.fn.col('.') - 1
+  return col == 0 or vim.fn.getline('.'):sub(col, col):match('%s')
 end
 
 local kind_icons = {
-  Text = "",
-  Method = "m",
-  Function = "",
-  Constructor = "",
-  Field = "",
-  Variable = "",
-  Class = "",
-  Interface = "",
-  Module = "",
-  Property = "",
-  Unit = "",
-  Value = "",
-  Enum = "",
-  Keyword = "",
-  Snippet = "",
-  Color = "",
-  File = "",
-  Reference = "",
-  Folder = "",
-  EnumMember = "",
-  Constant = "",
-  Struct = "",
-  Event = "",
-  Operator = "",
-  TypeParameter = "",
+  Text = '',
+  Method = 'm',
+  Function = '',
+  Constructor = '',
+  Field = '',
+  Variable = '',
+  Class = '',
+  Interface = '',
+  Module = '',
+  Property = '',
+  Unit = '',
+  Value = '',
+  Enum = '',
+  Keyword = '',
+  Snippet = '',
+  Color = '',
+  File = '',
+  Reference = '',
+  Folder = '',
+  EnumMember = '',
+  Constant = '',
+  Struct = '',
+  Event = '',
+  Operator = '',
+  TypeParameter = '',
 }
 
-cmp.setup {
+cmp.setup({
   snippet = {
     expand = function(args)
       luasnip.lsp_expand(args.body) -- For `luasnip` users.
     end,
   },
   mapping = {
-    ["<C-k>"] = cmp.mapping.select_prev_item(),
-    ["<C-j>"] = cmp.mapping.select_next_item(),
-    ["<C-u>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
-    ["<C-d>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
-    ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
-    ["<C-y>"] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
-    ["<C-e>"] = cmp.mapping {
+    ['<C-k>'] = cmp.mapping.select_prev_item(),
+    ['<C-j>'] = cmp.mapping.select_next_item(),
+    ['<C-u>'] = cmp.mapping(cmp.mapping.scroll_docs(-1), { 'i', 'c' }),
+    ['<C-d>'] = cmp.mapping(cmp.mapping.scroll_docs(1), { 'i', 'c' }),
+    ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
+    ['<C-y>'] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
+    ['<C-e>'] = cmp.mapping({
       i = cmp.mapping.abort(),
       c = cmp.mapping.close(),
-    },
+    }),
     -- Accept currently selected item. If none selected, `select` first item.
     -- Set `select` to `false` to only confirm explicitly selected items.
-    ["<CR>"] = cmp.mapping.confirm { select = true },
-    ["<Tab>"] = cmp.mapping(function(fallback)
+    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+    ['<Tab>'] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
       elseif luasnip.expandable() then
@@ -179,10 +178,10 @@ cmp.setup {
         fallback()
       end
     end, {
-      "i",
-      "s",
+      'i',
+      's',
     }),
-    ["<S-Tab>"] = cmp.mapping(function(fallback)
+    ['<S-Tab>'] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_prev_item()
       elseif luasnip.jumpable(-1) then
@@ -191,38 +190,41 @@ cmp.setup {
         fallback()
       end
     end, {
-      "i",
-      "s",
+      'i',
+      's',
     }),
   },
   formatting = {
-    fields = { "kind", "abbr", "menu" },
+    fields = { 'kind', 'abbr', 'menu' },
     format = function(entry, vim_item)
       -- Kind icons
-      vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
+      vim_item.kind = string.format('%s', kind_icons[vim_item.kind])
       -- This concatonates the icons with the name of the item kind
       -- vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind)
       vim_item.menu = ({
-        nvim_lsp = "[LSP]",
-        nvim_lua = "[NVIM_LUA]",
-        luasnip = "[Snippet]",
-        buffer = "[Buffer]",
-        path = "[Path]",
-        git = "[Git]",
+        nvim_lsp = '[LSP]',
+        nvim_lua = '[NVIM_LUA]',
+        luasnip = '[Snippet]',
+        buffer = '[Buffer]',
+        path = '[Path]',
+        git = '[Git]',
       })[entry.source.name]
       return vim_item
     end,
   },
   sources = {
-    { name = "nvim_lsp" },
-    { name = "nvim_lua" },
-    { name = "luasnip" },
-    { name = "buffer",
+    { name = 'nvim_lsp' },
+    { name = 'nvim_lua' },
+    { name = 'luasnip' },
+    {
+      name = 'buffer',
       option = {
-        get_bufnrs = function() return vim.api.nvim_list_bufs() end
-      }
+        get_bufnrs = function()
+          return vim.api.nvim_list_bufs()
+        end,
+      },
     },
-    { name = "path" },
+    { name = 'path' },
   },
   confirm_opts = {
     behavior = cmp.ConfirmBehavior.Replace,
@@ -230,21 +232,21 @@ cmp.setup {
   },
   window = {
     documentation = {
-      border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
+      border = { '╭', '─', '╮', '│', '╯', '─', '╰', '│' },
     },
   },
   experimental = {
     ghost_text = true,
     native_menu = false,
   },
-}
+})
 
 cmp.setup.filetype('gitcommit', {
   sources = cmp.config.sources({
     { name = 'git' },
   }, {
     { name = 'buffer' },
-  })
+  }),
 })
 
 cmp.setup.filetype('octo', {
@@ -252,5 +254,5 @@ cmp.setup.filetype('octo', {
     { name = 'git' },
   }, {
     { name = 'buffer' },
-  })
+  }),
 })
