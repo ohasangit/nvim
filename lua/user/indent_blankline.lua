4537022428ba06aa281698ccfe05703d9e033e7b
Omar Hasan <omar.hasan@torc.ai>
2023-01-16 13:37:07 -0700

    Changing style for indent_blankline

--- lua/user/indent_blankline.lua
+++ lua/user/indent_blankline.lua
@@ -1,19 +1,9 @@
 vim.opt.termguicolors = true
-vim.cmd [[highlight IndentBlanklineContextStart guisp=#00FF00 gui=underdouble]]
+-- vim.cmd([[highlight IndentBlanklineContextStart guisp=#00FF00 gui=underdouble]])
 vim.opt.list = true
 
-require("indent_blankline").setup {
-    char = "",
-    char_highlight_list = {
-        "IndentBlanklineIndent1",
-        "IndentBlanklineIndent2",
-    },
-    space_char_highlight_list = {
-        "IndentBlanklineIndent1",
-        "IndentBlanklineIndent2",
-    },
-    show_trailing_blankline_indent = false,
-
-    show_current_context = true,
-    show_current_context_start = true,
-}
+require('indent_blankline').setup({
+  space_char_blankline = ' ',
+  show_current_context = true,
+  show_current_context_start = true,
+})
