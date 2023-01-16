a17339a540e5628c3d601ad7be7ffecd1493ea7a
Omar Hasan <omar.hasan@torc.ai>
2023-01-16 15:42:59 -0700

    Added spelling back

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -1,4 +1,5 @@
 local options = {
+  spell = true, -- spell
   backup = false, -- creates a backup file
   clipboard = "unnamedplus", -- allows neovim to access the system clipboard
   cmdheight = 1, -- more space in the neovim command line for displaying messages
