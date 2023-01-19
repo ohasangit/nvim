e399eeb7f7788b5fe3f5ef0d71bb4bc420a7019c
Omar Hasan <omar.hasan@torc.ai>
2023-01-19 13:39:46 -0500

    Linting

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -1,14 +1,14 @@
 local options = {
-  spell = true, -- spell
+  -- spell = true, -- spell
   backup = false, -- creates a backup file
-  clipboard = "unnamedplus", -- allows neovim to access the system clipboard
+  clipboard = 'unnamedplus', -- allows neovim to access the system clipboard
   cmdheight = 1, -- more space in the neovim command line for displaying messages
-  completeopt = { "menuone", "noselect" }, -- mostly just for cmp
+  completeopt = { 'menuone', 'noselect' }, -- mostly just for cmp
   conceallevel = 0, -- so that `` is visible in markdown files
-  fileencoding = "utf-8", -- the encoding written to a file
+  fileencoding = 'utf-8', -- the encoding written to a file
   hlsearch = true, -- highlight all matches on previous search pattern
   ignorecase = true, -- ignore case in search patterns
-  mouse = "a", -- allow the mouse to be used in neovim
+  mouse = 'a', -- allow the mouse to be used in neovim
   pumheight = 10, -- pop up menu height
   showmode = false, -- we don't need to see things like -- INSERT -- anymore
   showtabline = 0, -- always show tabs
@@ -30,18 +30,19 @@ local options = {
   relativenumber = false, -- set relative numbered lines
   numberwidth = 2, -- set number column width to 2 {default 4}
   textwidth = 100, -- set number column width to 2 {default 4}
-  signcolumn = "yes", -- always show the sign column, otherwise it would shift the text each time
+  signcolumn = 'yes', -- always show the sign column, otherwise it would shift the text each time
   wrap = false, -- display lines as one long line
   scrolloff = 8, -- is one of my fav
   sidescrolloff = 8,
-  guifont = "monospace:h17", -- the font used in graphical neovim applications
+  guifont = 'monospace:h17', -- the font used in graphical neovim applications
 }
 
-vim.opt.shortmess:append "c"
+vim.opt.shortmess:append('c')
 
 for k, v in pairs(options) do
   vim.opt[k] = v
 end
 
-vim.cmd "set whichwrap+=<,>,[,],h,l"
-vim.cmd [[set iskeyword+=-]]
+vim.cmd('set whichwrap+=<,>,[,],h,l')
+vim.cmd([[set iskeyword+=-]])
+vim.g.python3_host_prog = '/usr/bin/python3'
