76a05c1c1b42a83bba238351c39ea9051602c2ae
Omar Hasan <omar.hasan@torc.ai>
2023-03-16 12:05:20 -0600

    Trying out copilot in gitcommit...

--- lua/user/copilot.lua
+++ lua/user/copilot.lua
@@ -1,11 +1,16 @@
 local status_ok, copilot = pcall(require, 'copilot')
 if not status_ok then
+  print('Could not load copilot')
   return
 end
 
 copilot.setup({
   suggestion = { enabled = false },
   panel = { enabled = false },
+  filetypes = {
+    gitcommit = true,
+    yaml = true,
+  },
 })
 
 local status_ok_cmp, copilot_cmp = pcall(require, 'copilot_cmp')
