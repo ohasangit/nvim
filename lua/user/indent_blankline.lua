90029ce90881c46c4b6ae01b326ff14948f2a7d9
Omar Hasan <omar.hasan@torc.ai>
2023-07-06 17:02:41 -0600

    Added indent line plugin

--- lua/user/indent_blankline.lua
+++
@@ -1,9 +0,0 @@
-vim.opt.termguicolors = true
--- vim.cmd([[highlight IndentBlanklineContextStart guisp=#00FF00 gui=underdouble]])
-vim.opt.list = true
-
-require('indent_blankline').setup({
-  space_char_blankline = ' ',
-  show_current_context = true,
-  show_current_context_start = true,
-})
