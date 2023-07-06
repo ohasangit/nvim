06d9bc3804d2b79424e7072c31314a888a1f7f49
Omar Hasan <omar.hasan@torc.ai>
2023-07-06 16:53:57 -0600

    updating lazy options and adding lualine component

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -11,4 +11,17 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("plugins", {defaults = {version = "*"}})
+opts = {
+  defaults = {
+    version = "*"
+  },
+  checker = {
+    enabled = true,
+    notify = false
+  },
+  change_detection = {
+    notify = false
+  },
+}
+
+require("lazy").setup("plugins", opts)
