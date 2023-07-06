f77bc8783092ceca9473930ef0bb1f5d384e6c34
Omar Hasan <omar.hasan@torc.ai>
2023-07-06 16:38:16 -0600

    migrating lualine

---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,36 @@
+return {
+  'nvim-lualine/lualine.nvim',
+  dependencies = {
+    'nvim-tree/nvim-web-devicons'
+  },
+  opts = {
+    options = {
+      component_separators = { left = "", right = "" },
+      section_separators = { left = "", right = "" },
+    },
+    sections = {
+      lualine_a = { "mode" },
+      lualine_b = { "branch", "diff", "diagnostics" },
+      lualine_c = {
+        {
+          "filename", path = 3
+        }
+      },
+      lualine_x = { "encoding", "fileformat", "filetype" },
+      lualine_y = { "progress" },
+      lualine_z = { "location" },
+    },
+    inactive_sections = {
+      lualine_a = {},
+      lualine_b = {},
+      lualine_c = {
+        {
+          "filename", path = 3
+        }
+      },
+      lualine_x = { "location" },
+      lualine_y = {},
+      lualine_z = {},
+    },
+  }
+}
