ad1f676f2abdad9be75eaed821a720cf551fa7d7
Omar Hasan <omar.hasan@torc.ai>
2023-07-07 16:15:28 -0600

    single quotes

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,20 +1,21 @@
 return {
   'nvim-treesitter/nvim-treesitter',
-  branch = "master",
-  build = ":TSUpdate",
+  branch = 'master',
+  build = ':TSUpdate',
   opts = {
-    ensure_installed = {"lua", "python", "json", "bash", "c", "cmake", "cpp", "dockerfile", "diff", "git_config", "git_rebase", "gitcommit", "gitignore", "gitattributes", "http", "jq", "json", "json5", "yaml"},
+    ensure_installed = { 'lua', 'python', 'json', 'bash', 'c', 'cmake', 'cpp', 'dockerfile', 'diff', 'git_config',
+      'git_rebase', 'gitcommit', 'gitignore', 'gitattributes', 'http', 'jq', 'json', 'json5', 'yaml' },
     sync_install = false,
-    ignore_install = {},
+    ignore_install = {'comment'}, -- For some reason this parser has become super unperformant
     highlight = {
       enable = true,
       disable = {},
       additional_vim_regex_highlighting = true,
     },
-    indent = { enable = true, disable = { "yaml" } },
+    indent = { enable = true, disable = { 'yaml' } },
     rainbow = { enable = true, extended_mode = true }
   },
   config = function(_, opts)
-    require("nvim-treesitter.configs").setup(opts)
+    require('nvim-treesitter.configs').setup(opts)
   end
 }
