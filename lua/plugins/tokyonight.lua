f42298913da7175573d079a0d7021b997d6a567d
Omar Hasan <omar.hasan@torc.ai>
2023-07-07 16:58:21 -0600

    Cleaning up tokyonight plugin

--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -18,11 +18,6 @@ return {
       dim_inactive = true,
       lualine_bold = true,
 
-      --- You can override specific color groups to use other groups or a hex color
-      --- function will be called with a ColorScheme table
-      ---@param colors ColorScheme
-      on_colors = function(colors) end,
-
       --- You can override specific highlights to use other groups or a hex color
       --- function will be called with a Highlights and ColorScheme table
       ---@param hl Highlights
