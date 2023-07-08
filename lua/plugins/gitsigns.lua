8b506561c45c98a6269bf9c62221fffec0b96354
Omar Hasan <omar.hasan@torc.ai>
2023-07-07 22:59:47 -0600

    Add qf list for hunks

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -59,6 +59,7 @@ return {
       map('n', '<leader>hd', gs.diffthis)
       map('n', '<leader>hD', function() gs.diffthis('~') end)
       map('n', '<leader>td', gs.toggle_deleted)
+      map('n', '<leader>ha', ":Gitsigns setqflist<CR>")
 
       -- Text object
       map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
