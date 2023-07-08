4c7c2da017e2a8372011bae1be4dd7382cb37072
Omar Hasan <omar.hasan@torc.ai>
2023-07-07 22:25:05 -0600

    Fixing fugitive write

--- lua/plugins/git-fugitive.lua
+++ lua/plugins/git-fugitive.lua
@@ -1,7 +1,7 @@
 return {
   'tpope/vim-fugitive',
   keys = {
-    { "<leader>gw",  "<cmd>G write<cr>",               desc = "Git write" },
+    { "<leader>gw",  "<cmd>Gwrite<cr>",                desc = "Git write" },
     { "<leader>gc",  "<cmd>G commit<cr>",              desc = "Git commit" },
     { "<leader>gp",  "<cmd>G push<cr>",                desc = "Git push" },
     { "<leader>gpu", "<cmd>G push -u origin HEAD<cr>", desc = "Git push and track" },
