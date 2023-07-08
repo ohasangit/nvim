a8f3e37f0287de44d5c777070e2415a7c2430cc5
Omar Hasan <omar.hasan@torc.ai>
2023-07-07 22:23:17 -0600

    Don't need hover

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -5,7 +5,6 @@ return {
     local formatting = null_ls.builtins.formatting
     local diagnostics = null_ls.builtins.diagnostics
     local code_actions = null_ls.builtins.code_actions
-    local hover = null_ls.builtins.hover
     null_ls.setup({
       debug = false,
       sources = {
