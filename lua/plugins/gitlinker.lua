718bd3e4b5fe9f47435039d4de29a6681f4350ad
Omar Hasan <omar.hasan@torc.ai>
2023-07-10 11:48:08 -0600

    Adding more description to lazy

--- lua/plugins/gitlinker.lua
+++ lua/plugins/gitlinker.lua
@@ -1,13 +1,16 @@
 return {
   'ruifm/gitlinker.nvim',
-  keys = { '<leader>gY', { '<leader>gy', mode = 'v' } },
+  keys = {
+    { '<leader>gy', mode = 'n', desc = 'Browser navigate to file repo url' },
+    { '<leader>gy', mode = 'v', desc = 'Copy selected range url to clipboard' }
+  },
   dependencies = {
     'nvim-lua/plenary.nvim',
   },
   config = function()
     vim.keymap.set(
       'n',
-      '<leader>gY',
+      '<leader>gy',
       '<cmd>lua require"gitlinker".get_repo_url({action_callback = require"gitlinker.actions".open_in_browser})<cr>',
       { noremap = true, silent = true, desc = 'Browser navigate to file repo url' }
     )
