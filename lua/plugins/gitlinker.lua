6bc9cff3a3d9ad3a0ae7b9b431503336ab16f071
Omar Hasan <omar.hasan@torc.ai>
2023-07-10 11:44:06 -0600

    Added descriptions to keymaps

--- lua/plugins/gitlinker.lua
+++ lua/plugins/gitlinker.lua
@@ -9,13 +9,13 @@ return {
       'n',
       '<leader>gY',
       '<cmd>lua require"gitlinker".get_repo_url({action_callback = require"gitlinker.actions".open_in_browser})<cr>',
-      { noremap = true, silent = true }
+      { noremap = true, silent = true, desc = 'Browser navigate to file repo url' }
     )
     vim.keymap.set(
       'v',
       '<leader>gy',
       '<cmd>lua require"gitlinker".get_buf_range_url("v")<cr>',
-      { noremap = true }
+      { noremap = true, desc = 'Copy selected range url to clipboard' }
     )
   end,
 }
