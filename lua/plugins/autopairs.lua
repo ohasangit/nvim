e3adc410b7cc0b5e7a45ad191bc6f2120765121a
Omar Hasan <omar.hasan@torc.ai>
2023-07-12 15:42:51 -0600

    fix(autopairs): fixed autopair on fn completion

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -5,13 +5,13 @@ return {
     check_ts = true,
     fast_wrap = {}
   },
+  config = function(_, opts)
+    require('nvim-autopairs').setup(opts)
+    local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+    local cmp = require('cmp')
+    cmp.event:on(
+      'confirm_done',
+      cmp_autopairs.on_confirm_done()
+    )
+  end
 }
-
---[[ Check if this still does something
-local cmp_autopairs = require('nvim-autopairs.completion.cmp')
-local cmp_status_ok, cmp = pcall(require, 'cmp')
-if not cmp_status_ok then
-  return
-end
-cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done({ map_char = { tex = '' } }))
---]]
