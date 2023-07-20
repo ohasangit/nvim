745fc208e3181eeae94e4b6f8d5ce01d52ebf78a
Omar Hasan <omar.hasan@torc.ai>
2023-07-20 17:29:22 -0600

    refactor: Trim down plugins list and clean .gitignore
    Removed unused plugins from the 'plugins.lua' file in order to reduce clutter
     and improve performance. Also, removed the unused entry 'plugin/packer_comp
    iled.lua' from the .gitignore file.

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -1,53 +1,7 @@
--- Automatically install packer
-local install_path = vim.fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
-if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-  PACKER_BOOTSTRAP = vim.fn.system({
-    'git',
-    'clone',
-    '--depth',
-    '1',
-    'https://github.com/wbthomason/packer.nvim',
-    install_path,
-  })
-  print('Installing packer close and reopen Neovim...')
-  vim.cmd([[packadd packer.nvim]])
-end
-
--- Use a protected call so we don't error out on first use
-local status_ok, packer = pcall(require, 'packer')
-if not status_ok then
-  return
-end
-
 return packer.startup(function(use)
-  use('wbthomason/packer.nvim') -- Have packer manage itself
-  use('nvim-lua/popup.nvim')    -- An implementation of the Popup API from vim in Neovim
-  use('nvim-lua/plenary.nvim')  -- Useful lua functions used ny lots of plugins
-  use('windwp/nvim-autopairs')  -- Autopairs, integrates with both cmp and treesitter
-  use('numToStr/Comment.nvim')  -- Easily comment stuff
-  use('lewis6991/impatient.nvim')
-  use('ethanholz/nvim-lastplace')
-  use('kyazdani42/nvim-web-devicons')
-  use('kyazdani42/nvim-tree.lua')
   use('ellisonleao/glow.nvim')
-  use({
-    'nvim-lualine/lualine.nvim',
-    requires = { 'kyazdani42/nvim-web-devicons', opt = true },
-  })
-  use('folke/which-key.nvim')
   use({ 'iamcco/markdown-preview.nvim', run = 'cd app && yarn install' })
   use({ 'ckipp01/nvim-jenkinsfile-linter', requires = { 'nvim-lua/plenary.nvim' } })
-  use('lukas-reineke/indent-blankline.nvim')
-  use('NvChad/nvim-colorizer.lua')
-  use('eandrju/cellular-automaton.nvim')
-  use({
-    'jackMort/ChatGPT.nvim',
-    requires = {
-      'MunifTanjim/nui.nvim',
-      'nvim-lua/plenary.nvim',
-      'nvim-telescope/telescope.nvim',
-    },
-  })
   use('folke/neodev.nvim')
   use({
     'nvim-neotest/neotest',
@@ -60,65 +14,7 @@ return packer.startup(function(use)
   use({
     'nvim-neotest/neotest-python',
   })
-  use({
-    'kylechui/nvim-surround',
-    tag = '*', -- Use for stability; omit to use `main` branch for the latest features
-    config = function()
-      require('nvim-surround').setup({
-        -- Configuration here, or leave empty to use defaults
-      })
-    end,
-  })
   use({ 'michaelb/sniprun', run = 'bash ./install.sh' })
-  use {
-    'rmagatti/goto-preview',
-    config = function()
-      require('goto-preview').setup {}
-    end
-  }
-  -- Colorschemes
-  use('folke/tokyonight.nvim')
-  use('Abstract-IDE/Abstract-cs')
-  use('marko-cerovac/material.nvim')
-  use({
-    "neanias/everforest-nvim",
-    -- Optional; default configuration will be used if setup isn't called.
-    config = function()
-      require("everforest").setup({
-        background = "hard",
-      })
-    end,
-  })
-  -- cmp plugins
-  use('hrsh7th/nvim-cmp')
-  use('hrsh7th/cmp-buffer')
-  use('hrsh7th/cmp-path')
-  use('hrsh7th/cmp-cmdline')
-  use('saadparwaiz1/cmp_luasnip')
-  use('hrsh7th/cmp-nvim-lsp')
-  use('hrsh7th/cmp-nvim-lua')
-  use('petertriho/cmp-git')
-  use('zbirenbaum/copilot.lua')
-  use('zbirenbaum/copilot-cmp')
-
-  -- snippets
-  use('L3MON4D3/LuaSnip')
-  use('rafamadriz/friendly-snippets')
-
-  -- LSP
-  use('williamboman/mason.nvim')
-  use({ 'williamboman/mason-lspconfig.nvim' })
-  use('jose-elias-alvarez/null-ls.nvim') -- for formatters and linters
-  use({ 'neovim/nvim-lspconfig' })       -- Configurations for Nvim LSP
-  use({
-    'junnplus/lsp-setup.nvim',
-    requires = {
-      'neovim/nvim-lspconfig',
-      'williamboman/mason.nvim',
-      'williamboman/mason-lspconfig.nvim',
-    },
-  })
-  use({ 'folke/trouble.nvim' })
 
   -- Telescope
   use('nvim-telescope/telescope.nvim')
@@ -128,12 +24,6 @@ return packer.startup(function(use)
   use({ 'ahmedkhalf/project.nvim' })
   use({ 'nvim-telescope/telescope-dap.nvim' })
 
-  -- Treesitter
-  use({
-    'nvim-treesitter/nvim-treesitter',
-    run = ':TSUpdate',
-  })
-  use('p00f/nvim-ts-rainbow')
   use({
     'm-demare/hlargs.nvim',
     requires = { 'nvim-treesitter/nvim-treesitter' },
@@ -141,26 +31,6 @@ return packer.startup(function(use)
       require('hlargs').setup()
     end,
   })
-
-  -- Git
-  use('tpope/vim-fugitive')
-  use('ruifm/gitlinker.nvim')
-  use('lewis6991/gitsigns.nvim')
-  use({
-    'pwntester/octo.nvim',
-    requires = {
-      'nvim-lua/plenary.nvim',
-      'nvim-telescope/telescope.nvim',
-      'kyazdani42/nvim-web-devicons',
-    },
-    config = function()
-      require('octo').setup()
-    end,
-  })
-  use { 'akinsho/git-conflict.nvim', tag = "*" }
-
-  -- Web Development
-  use('ray-x/web-tools.nvim')
 
   -- DAP
   use('mfussenegger/nvim-dap')
@@ -174,10 +44,3 @@ return packer.startup(function(use)
   -- use('jay-babu/mason-nvim-dap.nvim')
   use('mfussenegger/nvim-dap-python')
   use('theHamsta/nvim-dap-virtual-text')
-
-  -- Automatically set up your configuration after cloning packer.nvim
-  -- Put this at the end after all plugins
-  if PACKER_BOOTSTRAP then
-    require('packer').sync()
-  end
-end)
