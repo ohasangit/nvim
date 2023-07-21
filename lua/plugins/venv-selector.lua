8d6e9274c802cd27f773451355149afc50e5dfc6
Omar Hasan <omar.hasan@torc.ai>
2023-07-21 16:21:25 -0600

    chore(lint): running formatter

--- lua/plugins/venv-selector.lua
+++ lua/plugins/venv-selector.lua
@@ -1,8 +1,8 @@
 return {
   'linux-cultist/venv-selector.nvim',
-  keys = {{
-		'<leader>vs', '<cmd>:VenvSelect<cr>', desc = 'Select Virtualenv'
-  }},
+  keys = { {
+    '<leader>vs', '<cmd>:VenvSelect<cr>', desc = 'Select Virtualenv'
+  } },
   dependencies = { 'neovim/nvim-lspconfig', 'nvim-telescope/telescope.nvim' },
   opts = {
     search = false
