ae35b1d789f8c6b4aa37555f732f596997b29860
Omar Hasan <omar.hasan@torc.ai>
2023-07-28 22:42:56 -0600

    feat(neogit): Add diffview.nvim and integrate with neogit
    This commit adds diffview.nvim to the lazy-lock.json and integrates it with
     neogit. The neogit configuration has been updated to accommodate this integration
    , including the necessary dependencies and configuration details.

--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -1,8 +1,21 @@
 return {
   'NeogitOrg/neogit',
-  dependencies = 'nvim-lua/plenary.nvim',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'sindrets/diffview.nvim'
+  },
   keys = {
     { '<leader>gg', '<cmd>Neogit<cr>', desc = 'Neogit' },
   },
-  config = true
+  config = function()
+    require("neogit").setup({
+      telescope_sorter = function()
+        return require("telescope").extensions.fzf.native_fzf_sorter()
+      end,
+      disable_hint = true,
+      integrations = {
+        diffview = true
+      },
+    })
+  end
 }
