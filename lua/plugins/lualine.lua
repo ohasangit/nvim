297718189df670961112df01f2275076bc39ffbb
Omar Hasan <omar.hasan@torc.ai>
2023-12-23 14:35:49 -0700

    feat(lualine): improve lualine configuration and add copilot-lualine dependency

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,45 +1,69 @@
 return {
   'nvim-lualine/lualine.nvim',
   dependencies = {
-    'nvim-tree/nvim-web-devicons'
+    'nvim-tree/nvim-web-devicons',
+    'AndreM222/copilot-lualine'
   },
-  opts = {
-    options = {
-      component_separators = { left = '', right = '' },
-      section_separators = { left = '', right = '' },
-    },
-    sections = {
-      lualine_a = { 'mode' },
-      lualine_b = {
-        'branch',
-        'diff',
-        'diagnostics',
-        {
-          require('lazy.status').updates,
-          cond = require('lazy.status').has_updates,
-          color = { fg = '#ff9e64' },
+  config = function()
+    require('lualine').setup({
+      options = {
+        component_separators = { left = '', right = '' },
+        section_separators = { left = '', right = '' },
+      },
+      sections = {
+        lualine_a = { 'mode' },
+        lualine_b = {
+          'branch',
+          'diff',
+          'diagnostics',
+          {
+            require('lazy.status').updates,
+            cond = require('lazy.status').has_updates,
+            color = { fg = '#ff9e64' },
+          },
         },
+        lualine_c = {
+          {
+            'filename', path = 3
+          }
+        },
+        lualine_x = {
+          {
+            'copilot',
+            symbols = {
+              status = {
+                icons = {
+                  enabled = ' ',
+                  disabled = '󰜺 ',
+                  warning = ' ',
+                  unknown = ' '
+                },
+                hl = {
+                  enabled = '#50FA7B',
+                  disabled = '#6272A4',
+                  warning = '#FFB86C',
+                  unknown = '#FF5555',
+                }
+              },
+              spinners = require('copilot-lualine.spinners').dots,
+              spinner_color = '#50FA7B'
+            }
+          }, 'encoding', 'fileformat', 'filetype' },
+        lualine_y = { 'progress' },
+        lualine_z = { 'location' },
       },
-      lualine_c = {
-        {
-          'filename', path = 3
-        }
+      inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = {
+          {
+            'filename', path = 3
+          }
+        },
+        lualine_x = { 'location' },
+        lualine_y = {},
+        lualine_z = {},
       },
-      lualine_x = { 'encoding', 'fileformat', 'filetype' },
-      lualine_y = { 'progress' },
-      lualine_z = { 'location' },
-    },
-    inactive_sections = {
-      lualine_a = {},
-      lualine_b = {},
-      lualine_c = {
-        {
-          'filename', path = 3
-        }
-      },
-      lualine_x = { 'location' },
-      lualine_y = {},
-      lualine_z = {},
-    },
-  }
+    })
+  end,
 }
