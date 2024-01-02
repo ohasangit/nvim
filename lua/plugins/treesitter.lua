6f03e7b1ea858ab58dc49dd14df3565f009a6446
Omar Hasan <omar.hasan@torc.ai>
2024-01-01 18:08:10 -0700

    feat(treesitter): treesitter config update
    - Remove ts-rainbow due to bad performance
    - added jsonc for devcontainer json with comments

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,10 +1,11 @@
 return {
   'nvim-treesitter/nvim-treesitter',
+  branch = 'master',
   build = ':TSUpdate',
-  tag = 'v0.9.0',
   opts = {
     ensure_installed = { 'lua', 'python', 'json', 'bash', 'c', 'cmake', 'cpp', 'dockerfile', 'diff', 'git_config',
-      'git_rebase', 'gitcommit', 'gitignore', 'gitattributes', 'http', 'jq', 'json', 'json5', 'yaml', 'terraform',
+      'git_rebase', 'gitcommit', 'gitignore', 'gitattributes', 'http', 'jq', 'json', 'jsonc', 'json5', 'yaml',
+      'terraform',
       'javascript', 'markdown', 'groovy', 'toml', 'starlark' },
     sync_install = false,
     ignore_install = { 'comment' }, -- For some reason this parser has become super unperformant
@@ -14,11 +15,6 @@ return {
       additional_vim_regex_highlighting = true,
     },
     indent = { enable = true, disable = { 'yaml' } },
-    rainbow = { enable = true, extended_mode = true }
-  },
-  -- TODO: Move to https://github.com/hiphish/rainbow-delimiters.nvim
-  dependencies = {
-    'p00f/nvim-ts-rainbow'
   },
   config = function(_, opts)
     require('nvim-treesitter.configs').setup(opts)
