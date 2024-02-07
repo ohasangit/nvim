97fd2c039406080c5312d52e51d9330fe73abda3
Omar Hasan <omar.hasan@torc.ai>
2024-02-07 10:27:40 -0700

    feat(lualine): adding lsp progress plugin

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,7 +2,13 @@ return {
   'nvim-lualine/lualine.nvim',
   dependencies = {
     'nvim-tree/nvim-web-devicons',
-    'AndreM222/copilot-lualine'
+    'AndreM222/copilot-lualine',
+    {
+      'linrongbin16/lsp-progress.nvim',
+      config = function()
+        require('lsp-progress').setup()
+      end
+    }
   },
   config = function()
     require('lualine').setup({
@@ -25,6 +31,9 @@ return {
         lualine_c = {
           {
             'filename', path = 3
+          },
+          {
+            require('lsp-progress').progress,
           }
         },
         lualine_x = {
@@ -64,6 +73,14 @@ return {
         lualine_y = {},
         lualine_z = {},
       },
+    })
+
+    -- listen lsp-progress event and refresh lualine
+    vim.api.nvim_create_augroup("lualine_augroup", { clear = true })
+    vim.api.nvim_create_autocmd("User", {
+      group = "lualine_augroup",
+      pattern = "LspProgressStatusUpdated",
+      callback = require("lualine").refresh,
     })
   end,
 }
