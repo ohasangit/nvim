1ee7a1ceb6cd607ea4029be0fb0ae9e725c50eec
Omar Hasan <omar.hasan@torc.ai>
2024-02-22 18:08:10 -0700

    adding more neovide config

--- lua/config/neovide.lua
+++ lua/config/neovide.lua
@@ -1,5 +1,7 @@
 if vim.g.neovide then
   vim.o.guifont = "UbuntuMono Nerd Font:h13"
-  vim.g.neovide_transparency = 0.95
+  vim.g.neovide_transparency = 0.90
   vim.g.neovide_refresh_rate = 100
+  vim.g.neovide_hide_mouse_when_typing = true
+  vim.g.neovide_profiler = false
 end
