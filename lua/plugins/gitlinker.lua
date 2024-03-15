14455df5ab7e9d98a64d881954257448709c9260
Omar Hasan <omar.hasan@torc.ai>
2024-03-15 10:47:29 -0600

    feat(gitlinker): adding another shortcut for gitlinker

--- lua/plugins/gitlinker.lua
+++ lua/plugins/gitlinker.lua
@@ -2,6 +2,7 @@ return {
   'ruifm/gitlinker.nvim',
   keys = {
     { '<leader>gy', mode = 'n', desc = 'Browser navigate to file repo url' },
+    { '<leader>gY', mode = 'n', desc = 'Browser navigate to file repo url file' },
     { '<leader>gy', mode = 'v', desc = 'Copy selected range url to clipboard' }
   },
   dependencies = {
@@ -13,6 +14,12 @@ return {
       '<leader>gy',
       '<cmd>lua require"gitlinker".get_repo_url({action_callback = require"gitlinker.actions".open_in_browser})<cr>',
       { noremap = true, silent = true, desc = 'Browser navigate to file repo url' }
+    )
+    vim.keymap.set(
+      'n',
+      '<leader>gY',
+      '<cmd>lua require"gitlinker".get_buf_range_url("n", {action_callback = require"gitlinker.actions".open_in_browser})<cr>',
+      { noremap = true, silent = true, desc = 'Browser navigate to file repo url file' }
     )
     vim.keymap.set(
       'v',
