0a1585250444804175d2611a7efe78f49e896833
Omar Hasan <omar.hasan@torc.ai>
2024-03-26 11:03:20 -0600

    feat(neovide): Adding envs for neovide

--- lua/config/neovide.lua
+++ lua/config/neovide.lua
@@ -4,4 +4,13 @@ if vim.g.neovide then
   vim.g.neovide_refresh_rate = 100
   vim.g.neovide_hide_mouse_when_typing = true
   vim.g.neovide_profiler = false
+  vim.g.neovide_scroll_animation_far_lines = 0
+
+  local home_dir = vim.fn.getenv('HOME')
+  local path = vim.fn.getenv('PATH')
+
+  vim.fn.setenv("GOPATH", home_dir .. "/go")
+  vim.fn.setenv("PATH",
+    path .. ":/usr/local/go/bin:" .. home_dir .. "/.local/bin:" .. home_dir .. "/go/bin:" .. home_dir .. "/.tfenv/bin")
+  vim.fn.setenv("PYENV_ROOT", home_dir .. "/.pyenv")
 end
