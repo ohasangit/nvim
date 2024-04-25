ee68bcf7b67d0646e98569358b043b75bc3fb5d7
Omar Hasan <omar.hasan@torc.ai>
2024-04-25 08:30:17 -0600

    feat(octo): add pr keymap

--- lua/plugins/octo.lua
+++ lua/plugins/octo.lua
@@ -1,10 +1,11 @@
 return {
   'pwntester/octo.nvim',
   keys = {
-    { '<leader>op', '<cmd>Octo pr create<cr>', desc = 'Github Create PR' },
-    { '<leader>os', '<cmd>Octo search<cr>',    desc = 'Github Search' },
-    { '<leader>ol', '<cmd>Octo pr list<cr>',   desc = 'Github List PRs' },
-    { '<leader>om', '<cmd>Octo merge<cr>',     desc = 'Github Merge PR' },
+    { '<leader>op',  '<cmd>Octo pr create<cr>', desc = 'Github Create PR' },
+    { '<leader>opr', '<cmd>Octo pr<cr>',        desc = 'Github PR for current branch' },
+    { '<leader>os',  '<cmd>Octo search<cr>',    desc = 'Github Search' },
+    { '<leader>ol',  '<cmd>Octo pr list<cr>',   desc = 'Github List PRs' },
+    { '<leader>om',  '<cmd>Octo merge<cr>',     desc = 'Github Merge PR' },
   },
   dependencies = {
     'nvim-lua/plenary.nvim',
