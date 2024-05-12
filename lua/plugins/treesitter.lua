62fcca574cb0715b83fce4fb318761b3e6ddc9c9
Omar Hasan <omar.hasan@torc.ai>
2024-05-12 14:05:18 -0600

    feat(treesitter): adding incremental selection

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,9 +3,37 @@ return {
   branch = 'master',
   build = ':TSUpdate',
   opts = {
-    ensure_installed = { 'lua', 'python', 'json', 'bash', 'c', 'cmake', 'cpp', 'dockerfile', 'diff', 'git_config',
-      'git_rebase', 'gitcommit', 'gitignore', 'gitattributes', 'http', 'jq', 'json', 'jsonc', 'json5', 'yaml',
-      'terraform', 'javascript', 'markdown', 'groovy', 'toml', 'starlark', 'vimdoc', 'go' },
+    ensure_installed = {
+      'lua',
+      'python',
+      'json',
+      'bash',
+      'c',
+      'cmake',
+      'cpp',
+      'dockerfile',
+      'diff',
+      'git_config',
+      'git_rebase',
+      'gitcommit',
+      'gitignore',
+      'gitattributes',
+      'http',
+      'jq',
+      'json',
+      'jsonc',
+      'json5',
+      'yaml',
+      'terraform',
+      'javascript',
+      'typescript',
+      'markdown',
+      'groovy',
+      'toml',
+      'starlark',
+      'vimdoc',
+      'go'
+    },
     sync_install = false,
     ignore_install = { 'comment' }, -- For some reason this parser has become super unperformant
     highlight = {
@@ -13,7 +41,19 @@ return {
       disable = {},
       additional_vim_regex_highlighting = true,
     },
-    indent = { enable = true, disable = { 'yaml' } },
+    indent = {
+      enable = true,
+      disable = { 'yaml' }
+    },
+    incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = "<C-space>",
+        node_incremental = "<C-space>",
+        scope_incremental = false,
+        node_decremental = "<bs>",
+      },
+    },
   },
   config = function(_, opts)
     require('nvim-treesitter.configs').setup(opts)
