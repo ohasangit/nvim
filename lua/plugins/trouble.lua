4eb6cbae8b6e70b72f6e18904c2595e00510f422
Omar Hasan <omar.hasan@torc.ai>
2024-06-10 17:58:23 -0600

    fix(trouble): fixing cmds

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -1,16 +1,27 @@
 return {
-  'folke/trouble.nvim',
-  branch = 'main',
+  "folke/trouble.nvim",
+  opts = {}, -- for default options, refer to the configuration section for custom setup.
+  cmd = "Trouble",
   keys = {
-    { '<leader>di', '<cmd>TroubleToggle<cr>',                       desc = 'Trouble Toggle' },
-    { '<leader>dw', '<cmd>TroubleToggle workspace_diagnostics<cr>', desc = 'Trouble toggle workspace' },
-    { '<leader>di', '<cmd>TroubleToggle document_diagnostics<cr>',  desc = 'Trouble toggle document' },
-    { '<leader>dl', '<cmd>TroubleToggle loclist<cr>',               desc = 'Trouble toggle loclist' },
-    { '<leader>dq', '<cmd>TroubleToggle quickfix<cr>',              desc = 'Trouble toggle quickfix' },
-    { '<leader>dg', '<cmd>TroubleToggle lsp_references<cr>',        desc = 'Trouble toggle lsp references' }
+    {
+      "<leader>di",
+      "<cmd>Trouble diagnostics toggle<cr>",
+      desc = "Diagnostics (Trouble)",
+    },
+    {
+      "<leader>dI",
+      "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+      desc = "Buffer Diagnostics (Trouble)",
+    },
+    {
+      "<leader>dis",
+      "<cmd>Trouble symbols toggle focus=false<cr>",
+      desc = "Symbols (Trouble)",
+    },
+    {
+      "<leader>dil",
+      "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+      desc = "LSP Definitions / references / ... (Trouble)",
+    },
   },
-  opts = {
-    height = 25,
-    use_diagnostic_signs = true,
-  }
 }
