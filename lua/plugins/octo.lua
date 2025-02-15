9afd31f01d271d2ef7d164061dbb96045287b3a0
Omar Hasan <omar.hasan@torc.ai>
2025-02-14 17:18:29 -0700

    octo stuff

--- lua/plugins/octo.lua
+++ lua/plugins/octo.lua
@@ -5,12 +5,19 @@ return {
     { '<leader>opr', '<cmd>Octo pr<cr>',        desc = 'Github PR for current branch' },
     { '<leader>os',  '<cmd>Octo search<cr>',    desc = 'Github Search' },
     { '<leader>ol',  '<cmd>Octo pr list<cr>',   desc = 'Github List PRs' },
-    { '<leader>om',  '<cmd>Octo merge<cr>',     desc = 'Github Merge PR' },
+    { '<leader>om',  '<cmd>Octo pr merge squash<cr>',     desc = 'Github Merge PR' },
   },
   dependencies = {
     'nvim-lua/plenary.nvim',
     'nvim-telescope/telescope.nvim',
     'nvim-tree/nvim-web-devicons',
   },
-  config = true
+  opts = {
+    suppress_missing_scope = {
+      projects_v2 = true,
+    }
+  },
+  config = function(_, opts)
+    require('octo').setup(opts)
+  end,
 }
