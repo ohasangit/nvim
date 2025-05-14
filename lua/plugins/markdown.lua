097bfce35232443408d8a9044639754d0e7e2c0e
Omar Hasan <omar.hasan@torc.ai>
2025-05-13 18:37:05 -0600

    tagging markdown until stable

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,6 +1,7 @@
 return {
   'MeanderingProgrammer/markdown.nvim',
   dependencies = { 'nvim-treesitter/nvim-treesitter' },
+  tag = 'v8.3.0',
   config = function()
     require('render-markdown').setup({
       file_types = { 'markdown', 'md', 'help', 'copilot-chat' },
