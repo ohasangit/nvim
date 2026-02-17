f963aa2f3bd01b5d692158c880cac4ac86e30f7b
Omar Hasan <omar.hasan@torc.ai>
2026-02-17 09:17:09 -0700

    linting

--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -2,20 +2,20 @@ return {
   'NeogitOrg/neogit',
   dependencies = {
     'nvim-lua/plenary.nvim',
-    'sindrets/diffview.nvim'
+    'sindrets/diffview.nvim',
   },
   keys = {
     { '<leader>gg', '<cmd>Neogit<cr>', desc = 'Neogit' },
   },
   config = function()
-    require("neogit").setup({
+    require('neogit').setup({
       telescope_sorter = function()
-        return require("telescope").extensions.fzf.native_fzf_sorter()
+        return require('telescope').extensions.fzf.native_fzf_sorter()
       end,
       disable_hint = true,
       integrations = {
-        diffview = true
+        diffview = true,
       },
     })
-  end
+  end,
 }
